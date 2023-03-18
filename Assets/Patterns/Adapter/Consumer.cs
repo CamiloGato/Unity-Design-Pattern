b1c18b09fbe79ff7cb9a2751cfed3160f04a43c5
Camilo Andrés Rojas Hernández <cm.andres022@hotmail.com>
2023-03-18 11:10:45 -0500

    Adapter Pattern
    Example Using Adapter Pattern
    Creation of:
    - Consumer
    - Data
    - Data Store Interface
    - Data Store Implementation using
        * FileData Json
        * PlayerPrefs Unity

---
+++ Assets/Patterns/Adapter/Consumer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Patterns.Adapter
+{
+    public class Consumer : MonoBehaviour
+    {
+        
+        private IDataStore _fileDataStoreAdapter;
+        
+        private void Awake()
+        {
+            _fileDataStoreAdapter = new PlayerPrefsDataStoreAdapter();
+            var data = new Data("data1", "123");
+            _fileDataStoreAdapter.SetData(data, "Data1");
+        }
+
+        private void Start()
+        {
+            var data = _fileDataStoreAdapter.GetData<Data>("Data1");
+            print(data.data1);
+            print(data.data2);
+        }
+    }
+}
---
+++ Assets/Patterns/Adapter/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Patterns.Adapter
+{
+    [Serializable]
+    public class Data
+    {
+        public string data1;
+        public string data2;
+        
+        public Data(string data1, string data2)
+        {
+            this.data1 = data1;
+            this.data2 = data2;
+        }
+        
+    }
+}
