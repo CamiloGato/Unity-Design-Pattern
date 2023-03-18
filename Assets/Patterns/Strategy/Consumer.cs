11f61f8c194bf6cdaefb8ae9d40a10a80da44631
Camilo Andrés Rojas Hernández <cm.andres022@hotmail.com>
2023-03-18 14:15:19 -0500

    Strategy Update
    Creation Strategy Folder and Consumer
    - Creation Consumer
    - Creation Consume Installer
        * Random generator for Data Store

---
+++ Assets/Patterns/Strategy/Consumer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Patterns.Adapter;
+using UnityEngine;
+
+namespace Patterns.Strategy
+{
+    public class Consumer
+    {
+        private readonly IDataStore _dataStore;
+
+        public Consumer(IDataStore dataStore)
+        {
+            _dataStore = dataStore;
+        }
+
+        public void Save()
+        {
+            var data = new Data("Data2", "1234");
+            _dataStore.SetData(data, "Data2");
+        }
+        
+        public void Load()
+        {
+            var data = _dataStore.GetData<Data>("Data2");
+            Debug.Log(data.data1);
+            Debug.Log(data.data2);
+        }
+    }
+}
---
+++ Assets/Patterns/Strategy/ConsumerInstaller.cs
@@ -0,0 +1,27 @@
+﻿using Patterns.Adapter;
+using UnityEngine;
+
+namespace Patterns.Strategy
+{
+    public class ConsumerInstaller : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var consumer = new Consumer(GetDataStore());
+            consumer.Save();
+            consumer.Load();
+        }
+
+        private IDataStore GetDataStore()
+        {
+            var isEven = Random.Range(0, 2) % 2 == 0;
+            if (isEven)
+            {
+                return new PlayerPrefsDataStoreAdapter();
+            }
+            
+            return new FileDataStoreAdapter();
+            
+        }
+    }
+}
